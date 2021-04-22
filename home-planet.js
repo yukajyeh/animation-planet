ff029e0cbfab8737f48b660a2c13a1b5806717c4
Yuka Yeh <yukaka210@gmail.com>
2021-04-22 20:56:33 +0200

    eyeball movements

---
+++ home-planet.js
@@ -0,0 +1,10 @@
+const eyesTimeline = gsap.timeline({
+    repeat : -1
+})
+
+const eyeballs = document.querySelectorAll('path#ball, path#ball_2, path#ball_3, path#ball_4, path#ball_5, path#ball_6')
+
+eyesTimeline
+    .set(eyeballs, { y : 0 })
+    .to(eyeballs, { y : 7, duration: 0.25, dealy: 2, stagger: 0.25  })
+    .to(eyeballs, { y: 0, duration: 4 })
