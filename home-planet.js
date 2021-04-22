c2c8ac31ce9e8c65020ffe77c29a60da2202bd76
Yuka Yeh <yukaka210@gmail.com>
2021-04-22 21:35:08 +0200

    left,right arm animation

--- home-planet.js
+++ home-planet.js
@@ -8,3 +8,38 @@ eyesTimeline
     .set(eyeballs, { y : 0 })
     .to(eyeballs, { y : 7, duration: 0.25, dealy: 2, stagger: 0.25  })
     .to(eyeballs, { y: 0, duration: 4 })
+
+const hatTimeline = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 4
+})
+
+const hat = document.querySelector('g#hat')
+
+hatTimeline
+    .set(hat, { y:0 })
+    .to(hat, { y: -50, rotation: -10, duration: 0.25, delay: 1 })
+    .to(hat, { y: 0, rotation:0, duration: 0.5, delay: 0.1 })
+
+const leftArmTimeline = gsap.timeline({
+    repeat: -1
+})
+
+const leftArm = document.querySelector('#left-arm')
+
+leftArmTimeline
+    .set(leftArm, { rotation:0 })
+    .to(leftArm, { rotation:20, duration:0.25, delay:2 })
+    .to(leftArm, { rotation:0, duration:0.25, delay:2 })
+
+
+const rightArmTimeline = gsap.timeline({
+    repeat: -1
+})
+
+const rightArm = document.querySelector('#right-arm')
+
+rightArmTimeline
+    .set(rightArm, { rotation:0 })
+    .to(rightArm, { rotation:-20, duration:0.25, delay:3 })
+    .to(rightArm, { rotation:0, duration:0.25, delay:2 })
